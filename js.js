c8190bd63a300fb1ff72fff7b30f210d116c6ada
ronersonsprando <125032380+ronersonsprando@users.noreply.github.com>
2023-02-10 11:39:27 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,7 @@
+if (window.matchMedia('(max-width:500px)').matches) {
+  document.getElementById('agua').remove();
+} 
+
+if (window.matchMedia('(max-width:800px)').matches) {
+  document.getElementById('br').remove();
+} 
